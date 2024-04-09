684412871ff701595dbb727bdaeba5859ada02bf
ashleykylle <ashley_kylle_ramos@dlsu.edu.ph>
2024-04-09 11:12:08 +0800

    comment fixes and added datasets

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Set the directory for views
 app.set('views', path.join(__dirname, 'views'));
 
-// Set EJS as the template engine
 app.set('view engine', 'ejs');
 
-// Set up static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route handler for the root path
 app.get('/', (req, res) => {
   res.render('mainPage', { title: 'Transaction Management' });
 });
 
-// Route handlers for other paths
 app.get('/addApp', (req, res) => {
   res.render('addApp', { title: 'Add Appointment' });
 });
@@ -33,7 +28,6 @@ app.get('/deleteApp', (req, res) => {
   res.render('deleteApp', { title: 'Delete Appointment' });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
