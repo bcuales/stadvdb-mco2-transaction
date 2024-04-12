510ea1b8f4e4cadeea6c16af32afc3e45e89babf
ashleykylle <ashley_kylle_ramos@dlsu.edu.ph>
2024-04-12 15:21:34 +0800

    commented changes for replication (comments under 'CHANGE:')

--- db.js
+++ db.js
@@ -39,6 +39,10 @@ const poolConfig3 = {
   queueLimit: 0
 };
 
+//CHANGEs:
+// const masterPool = mysql.createPool(poolConfig1).promise();
+// const luzonPool = mysql.createPool(poolConfig2).promise();
+// const visminPool = mysql.createPool(poolConfig3).promise();
 const pool1 = mysql.createPool(poolConfig1).promise();
 const pool2 = mysql.createPool(poolConfig2).promise();
 const pool3 = mysql.createPool(poolConfig3).promise();
@@ -60,6 +64,9 @@ module.exports = {
   deleteAppointmentById,
   editAppointment,
   searchAppointmentById 
+  // masterPool
+  // luzonPool
+  // visminPool
 };
 
 async function createAppointment(appointmentDetails) {
@@ -74,6 +81,8 @@ async function createAppointment(appointmentDetails) {
         // Ensure appointmentId is the first value in the values array
         const values = [appointmentId, appointmentType, appointmentMode, doctorsSpecialty, hospitalOrClinic, hospitalRegion, hospitalProvince, hospitalCity, hospitalName, patientSex, patientAge];
         
+        // CHANGE:
+        // const [result] = await masterPool.query(query, values);
         const [result] = await pool1.query(query, values);
         return result;
     } catch (error) {
@@ -85,6 +94,8 @@ async function createAppointment(appointmentDetails) {
 
 async function deleteAppointmentById(appointmentId) {
     try {
+        // CHANGE:
+        // const [result, _] = await masterPool.query('DELETE FROM mco2_appts WHERE appointment_id = ?', [appointmentId]);
         const [result, _] = await pool1.query('DELETE FROM mco2_appts WHERE appointment_id = ?', [appointmentId]);
         return result;
     } catch (error) {
@@ -109,6 +120,8 @@ async function editAppointment(formData) {
         } = formData;
 
         // Update the appointment in the database using SQL queries
+        // CHANGE: 
+        // const result = await masterPool.query('UPDATE mco2_appts SET appointment_type = ?, mode_of_appointment = ?, doctor_specialty = ?, hospital_or_clinic_choice = ?, hospital_or_clinic_region = ?, hospital_or_clinic_province = ?, hospital_or_clinic_city = ?, patient_sex = ?, patient_age = ? WHERE appointment_id = ?', [appointmentType, appointmentMode, doctorsSpecialty, hospitalOrClinic, hospitalRegion, hospitalProvince, hospitalCity, patientSex, patientAge, appointmentID]);
         const result = await pool1.query('UPDATE mco2_appts SET appointment_type = ?, mode_of_appointment = ?, doctor_specialty = ?, hospital_or_clinic_choice = ?, hospital_or_clinic_region = ?, hospital_or_clinic_province = ?, hospital_or_clinic_city = ?, patient_sex = ?, patient_age = ? WHERE appointment_id = ?', [appointmentType, appointmentMode, doctorsSpecialty, hospitalOrClinic, hospitalRegion, hospitalProvince, hospitalCity, patientSex, patientAge, appointmentID]);
 
         // Check if the appointment was successfully edited
@@ -121,6 +134,7 @@ async function editAppointment(formData) {
   // Function to search appointment by ID across all databases
 async function searchAppointmentById(appointmentId) {
     try {
+        // const [results, _] = await masterPool.query('SELECT * FROM mco2_appts WHERE appointment_id = ?', [appointmentId]);
         const [results, _] = await pool1.query('SELECT * FROM mco2_appts WHERE appointment_id = ?', [appointmentId]);
         return results;
     } catch (error) {
